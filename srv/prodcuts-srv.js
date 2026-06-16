c76a51a16cf1f18114e5e6c20da743034ddb74a2
MukulVerma97 <mukulverma9731@gmail.com>
2026-06-16 17:43:15 +0000

    product-srv

---
+++ srv/prodcuts-srv.js
@@ -0,0 +1,18 @@
+const cap = require("@sap/cds");
+const { INSERT } = require("@sap/cds/lib/ql/cds-ql");
+const { request } = require("express");
+
+module.exports = class ProductsMgmtService extends cds.ApplicationService{
+    init(){
+        const { Products } = this.entities(ProductsMgmtService);
+
+    //  this.on('CREATE',Products, async req=>{
+
+    //   await  INSERT.into(Products).entries(req.data)
+      
+    //  })
+
+    
+        return super.init()
+    } 
+}
